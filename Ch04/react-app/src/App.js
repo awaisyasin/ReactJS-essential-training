1cc087428fa6a2f4d3705d0e92afef63f300be07
awaisyasin <awaisyasin.co@gmail.com>
2024-03-14 02:06:26 +0500

    Understanding the useState hook

--- Ch04/react-app/src/App.js
+++ Ch04/react-app/src/App.js
@@ -1,9 +1,13 @@
 import './App.css'
+import { useState } from 'react'
 
-function App({ library }) {
+function App() {
+    const [emotion, setEmotion] = useState('Happy')
     return (
         <div className='App'>
-            <h1>Getting started with {library}</h1>
+            <h1>Current emotion is {emotion}</h1>
+            <button onClick={() => setEmotion('Sad')}>Sad</button>
+            <button onClick={() => setEmotion('Exited')}>Exited</button>
         </div>
     )
 }
