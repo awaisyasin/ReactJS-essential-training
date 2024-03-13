868dfb6f5a9753d8875a5bcc762b856ba2444743
awaisyasin <awaisyasin.co@gmail.com>
2024-03-14 01:51:07 +0500

    Creating a React app

---
+++ Ch04/react-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App({ library }) {
+    return (
+        <div className='App'>
+            <h1>Getting started with {library}</h1>
+        </div>
+    )
+}
+
+export default App
+
