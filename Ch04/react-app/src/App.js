d0e8a2ecd761e252f4c2b9e3210102bdac158988
awaisyasin <awaisyasin.co@gmail.com>
2024-03-15 18:01:51 +0500

    Handling loading states

--- Ch04/react-app/src/App.js
+++ Ch04/react-app/src/App.js
@@ -1,22 +1,46 @@
 import './App.css'
 import { useState, useEffect } from 'react'
 
+function GithubUser({ name, bio, blog, avatar }) {
+    return (
+        <div>
+            <h1>{name}</h1>
+            <p>{bio}</p>
+            <p>{blog}</p>
+            <img src={avatar} alt={name} height={150}></img>
+        </div>
+    )
+}
+
 function App() {
     const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
     useEffect(() => {
+        setLoading(true)
         fetch('https://api.github.com/users/awaisyasin')
-        .then((response) => response.json())
-        .then(setData)
+            .then(response => response.json())
+            .then(setData)
+            .catch(setError)
+            .finally(() => setLoading(false))
     }, [])
 
-    if (data) {
-        return <pre>{JSON.stringify(data, null, 4)}</pre>
+    if (loading) {
+        return <h1>{loading}</h1>
     }
+    if (error) {
+        return <pre>{JSON.stringify(error)}</pre>
+    }
+    if (!data) return null
 
     return (
-        <div className='App'>
-            <h1>Data</h1>
-        </div>
+        <GithubUser
+            name={data.name}
+            bio={data.bio}
+            blog={data.blog}
+            avatar={data.avatar_url}
+        />
     )
 }
 
