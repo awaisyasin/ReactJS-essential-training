25723dcdc6b0efda1d24ab9d5b870594f39e7b92
awaisyasin <awaisyasin.co@gmail.com>
2024-03-16 08:46:37 +0500

    Installing react router and configuring the router

--- Ch04/react-app/src/App.js
+++ Ch04/react-app/src/App.js
@@ -1,39 +1,28 @@
 import './App.css'
 
-const tahoe_peaks = [
-    { name: 'Freel', elevation: 10891 },
-    { name: 'Monument', elevation: 10067 },
-    { name: 'Pyramid', elevation: 9983 },
-    { name: 'Tallac', elevation: 9735 },
-]
-
-function App() {
-    function List({ data, renderItem, renderEmpty }) {
-        return !data.length ? (
-            renderEmpty
-        ) : (
-            <ul>
-                {data.map(item => (
-                    <li key={item.name}>{renderItem(item)}</li>
-                ))}
-            </ul>
-        )
-    }
-
+function Home() {
+    return <h1>Home Page</h1>
+}
+export function About() {
     return (
-        <div>
-            <List
-                data={tahoe_peaks}
-                renderEmpty={<h1>This list is empty</h1>}
-                renderItem={item => (
-                    <>
-                        {item.name} - {item.elevation} ft.
-                    </>
-                )}
-            />
+        <div className='App'>
+            <h1>About Us</h1>
+        </div>
+    )
+}
+export function Contact() {
+    return (
+        <div className='App'>
+            <h1>Contact Us</h1>
         </div>
     )
 }
 
-export default App
+export function App() {
+    return (
+        <div className='App'>
+            <Home />
+        </div>
+    )
+}
 
